68a867c581f8fe4a1b6f0ce40f64a902a4edb87e
Kulkodar <kevin.winkler@cn-consult.eu>
2021-10-13 15:26:38 +0200

    Change user customertree now uses a external ts file for users so the user info can be reused

--- angular/src/app/customertree/customertree.component.ts
+++ angular/src/app/customertree/customertree.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {users} from "../grid.tree/users";
 
 @Component({
   selector: 'syncfusion-demo-customertree',
@@ -7,55 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomertreeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    let userList = [];
+    for(let i = 0; i < users.length; i++)
+    {
+      userList.push({id:users[i].id,name:users[i].firstName + " " + users[i].lastName});
+    }
+
+    this.hierarchicalData = [ {id:"Customer", name:"Customer", expanded:true,
+        subChild: [
+          {id: 'users', name: 'Users',
+            subChild:userList},
+        ]
+        } ];
+
+    this.field = { dataSource: this.hierarchicalData, id: 'id', text: 'name', child: 'subChild' };
+  }
 
   ngOnInit(): void {
   }
-  public hierarchicalData: Object[] = [
-    { id: '01', name: 'Music', expanded: true,
-      subChild: [
-        {id: '01-01', name: 'Gouttes.mp3'},
-      ]
-    },
-    {
-      id: '02', name: 'Videos',
-      subChild: [
-        {id: '02-01', name: 'Naturals.mp4'},
-        {id: '02-02', name: 'Wild.mpeg'}
-      ]
-    },
-    {
-      id: '03', name: 'Documents',
-      subChild: [
-        {id: '03-01', name: 'Environment Pollution.docx'},
-        {id: '03-02', name: 'Global Water, Sanitation, & Hygiene.docx'},
-        {id: '03-03', name: 'Global Warming.ppt'},
-        {id: '03-02', name: 'Social Network.pdf'},
-        {id: '03-03', name: 'Youth Empowerment.pdf'},
-      ]
-    },
-    { id: '04', name: 'Music', expanded: true,
-      subChild: [
-        {id: '04-01', name: 'Gouttes.mp3'},
-      ]
-    },
-    {
-      id: '05', name: 'Videos',
-      subChild: [
-        {id: '05-01', name: 'Naturals.mp4'},
-        {id: '05-02', name: 'Wild.mpeg'}
-      ]
-    },
-    {
-      id: '06', name: 'Documents',
-      subChild: [
-        {id: '06-01', name: 'Environment Pollution.docx'},
-        {id: '06-02', name: 'Global Water, Sanitation, & Hygiene.docx'},
-        {id: '06-03', name: 'Global Warming.ppt'},
-        {id: '06-02', name: 'Social Network.pdf'},
-        {id: '06-03', name: 'Youth Empowerment.pdf'},
-      ]
-    }
-  ];
-  public field:Object ={ dataSource: this.hierarchicalData, id: 'id', text: 'name', child: 'subChild' };
+
+  public hierarchicalData: Object[] ;
+  public field:Object;
 }
